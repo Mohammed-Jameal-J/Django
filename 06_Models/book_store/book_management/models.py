543dd321d17c94cc7b53e09963aa69a0fc39b1b6
Mohammed Jameal <techprompt1031@gmail.com>
2026-05-04 23:51:56 +0530

    model

---
+++ 06_Models/book_store/book_management/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.id} - {self.name}"
+    
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    def __str__(self):
+        return f"{self.id} - {self.title}"
