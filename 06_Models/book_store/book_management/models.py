a57884d6d49e8085083301d8a6aedcb48852f45b
Mohammed Jameal <techprompt1031@gmail.com>
2026-05-05 00:35:29 +0530

    models version"2"

--- 06_Models/book_store/book_management/models.py
+++ 06_Models/book_store/book_management/models.py
@@ -7,10 +7,21 @@ class Author(models.Model):
 
     def __str__(self):
         return f"{self.id} - {self.name}"
+
+class AuthorProfile(models.Model):
+    bio = models.TextField(blank=True)
+    author = models.OneToOneField(Author, on_delete=models.CASCADE, related_name='profile')
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.id} - {self.name}"
     
 class Book(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books', default=1)
+    categories = models.ManyToManyField(Category, related_name='books')
     def __str__(self):
         return f"{self.id} - {self.title}"
