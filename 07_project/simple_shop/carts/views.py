17e783afd706e9ce1bbb31ae13b919a5ad4889c5
Mohammed Jameal <techprompt1031@gmail.com>
2026-05-08 16:12:40 +0530

    Cart Flow

--- 07_project/simple_shop/carts/views.py
+++ 07_project/simple_shop/carts/views.py
@@ -1,17 +1,55 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.status import status
+from rest_framework import status
 from .models import Cart, CartItem
 from .serializers import CartSerializer, CartItemSerializer
 
 
 # Create your views here.
 
-class CartViewSet(ModelViewSet):
+class CartViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
-    def get_cart(self,user):
+    def get_cart(self, user):
         cart, created = Cart.objects.get_or_create(user=user)
         return cart
+    @action(detail=False, methods=['get'])
+    def list(self, request):
+        cart = self.get_cart(request.user)
+        serializer = CartSerializer(cart)
+        return Response(serializer.data)
+    @action(detail=False, methods=['post'])
+    def add(self, request):
+        cart = self.get_cart(request.user)
+        serializer = CartItemSerializer(data=request.data)
+        if serializer.is_valid():
+            product = serializer.validated_data['product']
+            cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+            if not created:
+                cart_item.quantity += quantity
+            else:
+                cart_item.quantity = quantity
+            cart_item.save()
+            return Response(CartItemSerializer(cart_item).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    @action(detail=True, methods=['post'])
+    def update_item(self, request, pk=None):
+        cart = self.get_cart(request.user)
+        try:
+            cart_item = CartItem.objects.get(cart=cart, id=pk)
+        except CartItem.DoesNotExist:
+            return Response({'error': 'Cart item not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = CartItemSerializer(cart_item, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+        
